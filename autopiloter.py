8f6b533b331922fb7bde7fa92f44c82997064f47
MasqueradeOfSilence <alexcn711@gmail.com>
2024-10-17 11:38:46 -0600

    format

--- autopiloter.py
+++ autopiloter.py
@@ -322,5 +322,6 @@ elif today == "Sunday":
 else:
     print(no_tasks_found)
 
+print("Consider freelance work\n\n")
 print("If 2-minute rule is complete, make a check on your calendar!")
-print("If all tasks are completed to an adequate depth, put a star on top of the check!")
+print("If all tasks are completed to an adequate depth, put a star on top of the check!\n\n")
